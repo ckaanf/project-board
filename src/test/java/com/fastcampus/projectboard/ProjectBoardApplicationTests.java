b1e1f78df644b343f50f95011e44f130d2166a10
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-11-03 13:19:52 +0900

    #8- AuditingFields 추출
    생성자,생성일시,수정자,수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접 연관이 없는 요소이므로
    추출 가능
    `@MapperdSuperClass` 이용하여 상속방식으로 추출

--- src/test/java/com/fastcampus/projectboard/ProjectBoardApplicationTests.java
+++ src/test/java/com/fastcampus/projectboard/ProjectBoardApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class ProjectBoardApplicationTests {
 
     @Test
     void contextLoads() {
+
+
     }
 
 }
