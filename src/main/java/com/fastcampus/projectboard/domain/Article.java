5a7d4edacb6a0ae90b04d60ad3d22dd7ab6a303f
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-11-03 12:15:06 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1테이블 매칭되도록 설계

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -7,10 +7,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -20,6 +23,8 @@ import java.util.Objects;
         @Index(columnList ="createdAt"),
         @Index(columnList ="createdBy"),
 } )
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
     @Id
@@ -31,6 +36,11 @@ public class Article {
     @Setter @Column(nullable = false, length = 10000)private String content; //본문
 
     @Setter private String hashtag; //해시태그
+
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article" , cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     //Meta Data
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; //생성일시
