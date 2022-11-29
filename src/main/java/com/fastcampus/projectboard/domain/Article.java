765bb5db43a153225a108ba5788b59e28b7bf3f1
frontLine-kim <57103133+frontLine-kim@users.noreply.github.com>
2022-11-29 11:44:43 +0900

    # 31 -게시판 수정 삭제 구현

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -31,7 +31,7 @@ public class Article extends AuditingFields {
     private Long id;
 
     @Setter
-    @ManyToOne(optional = false) private UserAccount userAccount;  //유저 정보
+    @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount;  //유저 정보
 
     @Setter @Column(nullable = false) private String title;  //제목
     @Setter @Column(nullable = false, length = 10000)private String content; //본문
